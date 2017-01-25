49c06f16cd3b92d9307522c994a49fd7db168f48
roychamag <johanneswinkelmann@yahoo.de>
2017-01-25 11:28:10 +0200

    Add files via upload

--- Test/Window5.xaml.cs
+++ Test/Window5.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.OleDb;
 
 namespace Test
 {
@@ -19,9 +21,53 @@ namespace Test
     /// </summary>
     public partial class Window5 : Window
     {
+        Basisklasse bk = new Basisklasse();
+        OleDbDataReader dr;
         public Window5()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bk.Connection();
+                try
+                {
+                    dr = bk.Select("SELECT Last(P_Nr) FROM Personal;");
+                    dr.Read();
+                    try
+                    {
+                        int inter = dr.GetInt32(0);
+                        lAbtNr.Content = inter;
+                    }
+                    catch
+                    {
+                        lAbtNr.Content = "1";
+                    }
+                    
+                    bk.CloseCon();
+                }
+                catch { MessageBox.Show("Fehler beim bestimmen der Personal Nummer", "", MessageBoxButton.OK, MessageBoxImage.Error); bk.CloseCon(); return; }
+
+                try
+                {
+                    bk.Connection();
+                    dr = bk.Select("SELECT Abt_Bez FROM Abteilung;");
+                    while (dr.Read())
+                    {
+                        string bob = dr.GetString(1);
+                        cbAbtNr.Items.Add(bob);
+                    }
+                    cbAbtNr.Items.Refresh();
+                    bk.CloseCon();
+
+                }
+                catch { MessageBox.Show("Fehler beim Bestimmen der Abteilungen", "", MessageBoxButton.OK, MessageBoxImage.Error); bk.CloseCon(); return; }
+
+            }
+            catch { MessageBox.Show("Die Verbindung zur Datenbank konnte nicht hergestellt werden.", "", MessageBoxButton.OK, MessageBoxImage.Error); }
         }
     }
 }
